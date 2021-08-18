49cd92b683ff23d9543bbf75b64d78ae38653f79
Ian F <ian.fiddeman@gmail.com>
2021-08-18 15:02:36 +0100

    show paragraph now changes using state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 import './App.css';
 
@@ -16,8 +17,8 @@ const toggleParagraphHandler = () => {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
-      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
